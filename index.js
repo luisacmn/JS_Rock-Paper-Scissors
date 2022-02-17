9ae37cca3202c21abd6ed6dd623f03f76a0c2d07
Luisa Novaes <luisanovaes.arq@gmail.com>
2022-02-17 16:55:11 -0300

    Finished the project and uploaded the files

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const btns = document.querySelectorAll(".btn")
 const playerTxt = document.querySelector(".player-txt")
 const computerTxt = document.querySelector(".computer-txt")
-const playBtn = document.querySelector(".play-btn");
-const pcBtn = document.querySelectorAll(".pc-btn")
+const pcBtns = document.querySelectorAll(".pc-btn")
 const resultTxt = document.querySelector(".main-result")
 const playerBox = document.querySelector(".player-box")
 const playerImg = document.querySelector(".player-img")
@@ -10,33 +9,37 @@ const computerBox = document.querySelector(".computer-box")
 const computerImg = document.querySelector(".computer-img")
 const pscore = document.querySelector(".player-score")
 const cscore = document.querySelector(".computer-score")
+const playContainer = document.querySelector(".play-container")
+const playBtn = document.querySelector(".play-btn");
+const restartBtn = document.querySelector(".restart-btn")
+const game = document.querySelector(".game")
+const body = document.querySelector(".body-page")
 
 
-btns.forEach (function (i) {                                            //Para cada botão btn do Player, adicionar a função click
+btns.forEach (function (i) {                                        //For each Player button (btn), add a click function 
     i.addEventListener("click", userPlay)
 })
 
-pcBtn.forEach (function (i) {                                           //Desativar todos os botões do pc
+pcBtns.forEach (function (i) {                                       //Disable all the pc buttons
     i.disabled = true;
 })                                         
 
-let playerSelection =""
+let playerSelection =""                                             //Declare a variable to be changed in the future
 let computerSelection =""
 
-const hands = ["Rock", "Paper", "Scissors"] 
+const hands = ["Rock", "Paper", "Scissors"]                         //Declare the game options inside and array
 
-let newScoreP = 0;
+let newScoreP = 0;                                                  //Declare a variable to count and change the score
 let newScoreC = 0;
-
 
 
 //PLAYER
 function userPlay(event){     
-    playerSelection = event.target.value;                           //função click detecta o valor selecionado e escreve no parágrafo
-    playerTxt.innerHTML = "You chose " + playerSelection;
+    playerSelection = event.target.value;                           //The Player selection receive the value of the event(click) target. 
+    playerTxt.innerHTML = "You chose " + playerSelection + " !";    //Write this value on the paragraph
+    event.target.classList.add("selected")                          //Add a class to target manipulate its style in css when the button is clicked
 
-
-    if(playerSelection === "Rock") {
+    if(playerSelection === "Rock") {                                //Execute different actions based on the condition
         playerImg.src="images/rock.png";
         playerBox.appendChild(playerImg)
     } else if (playerSelection === "Paper") {
@@ -48,15 +51,13 @@ function userPlay(event){
     }
 } 
 
-
-
 //COMPUTER
 function computerPlay() {
-    const randomNumber = Math.floor(Math.random() * 3);
-    computerSelection = hands[randomNumber];
-    computerTxt.innerHTML="Computer chose " + computerSelection;
+    const randomNumber = Math.floor(Math.random() * 3);                 //Sort a number from the array length. 
+    computerSelection = hands[randomNumber];                            //This number will be the index of the array to select an item.
+    computerTxt.innerHTML="Computer chose " + computerSelection + "!";
 
-    if(computerSelection === "Rock") {
+    if(computerSelection === "Rock") {                                  //Execute different actions based on the condition
         computerImg.src="images/rock.png";
         computerBox.appendChild(computerImg)
     } else if (computerSelection === "Paper") {
@@ -74,29 +75,83 @@ playBtn.addEventListener("click", playRound);
 
 function playRound() {
     computerPlay();
+    
     if (
         (playerSelection === "Rock" && computerSelection === "Scissors") ||
         (playerSelection === "Paper" && computerSelection === "Rock") ||
         (playerSelection === "Scissors" && computerSelection === "Paper")
         ) {
         resultTxt.innerHTML = "YOU WIN!";
-        newScoreP++;                                            //incrementa o score
-        pscore.innerHTML = "Score: " + newScoreP;               //escreve o novo socre no local seleciondo
+        newScoreP++;                                            //Increment the score
+        pscore.innerHTML = "Score: " + newScoreP;               //Write the new score in the selected place
     } else if (playerSelection === computerSelection) {
         resultTxt.innerHTML = "Its a TIE!";
     } else {
         resultTxt.innerHTML = "YOU LOSE!";
         newScoreC++;
         cscore.innerHTML = "Score: " + newScoreC;
-    }
+    } 
+
+    if(newScoreP === 5 || newScoreC === 5) {
+        gameOver()
+    } else {
+        newRound();  
+    }                                               
 }
 
 
+//NEW ROUND 
+function newRound(){
+    setTimeout(clear,2000)                                  //After 2 seconds, clear image and text 
+}
+
+function clear(){
+    playerBox.removeChild(playerImg);                       //Remove player image from player box
+    computerBox.removeChild(computerImg);                   //Remove computer image from computer box
+    playerTxt.innerHTML="";                                 //Remove texts by giving to these variables an empty string value
+    computerTxt.innerHTML="";
+    resultTxt.innerHTML="";
+} 
+resultTxt.innerHTML="";
 
 
+//RESTART
+restartBtn.addEventListener("click", restartGame);          //Add a event and function to the restart button
 
+function restartGame(){
+    clear();                                                  //Execute clear functin
+    pscore.innerHTML= "Score: 0"                              //Replace the score
+    cscore.innerHTML= "Score: 0"
+}
 
+//GAME OVER
+function gameOver(){
+    if(newScoreP > newScoreC) {
+        body.removeChild(game)
+        playContainer.removeChild(playBtn)
+        playContainer.removeChild(restartBtn)
+        resultTxt.innerHTML="Game Over. You WIN ! 🏆";
+        let newGameBtn = document.createElement("button")               //Create a new element
+        newGameBtn.textContent = "New Game"                             //Add text content to this new element
+        newGameBtn.classList.add("new-game")                            //Add a class to this element
+        playContainer.appendChild(newGameBtn)                           //Append this new element to its parent
+        newGameBtn.addEventListener("click", newGame);                  //Add an Event and its function to this new element
+    } else if (newScoreC > newScoreP) {
+        body.removeChild(game)
+        playContainer.removeChild(playBtn)
+        playContainer.removeChild(restartBtn)
+        resultTxt.innerHTML="Game Over. You LOSE ! 😞";
+        let newGameBtn = document.createElement("button")
+        newGameBtn.textContent = "New Game"
+        newGameBtn.classList.add("new-game")
+        playContainer.appendChild(newGameBtn)
+        newGameBtn.addEventListener("click", newGame);
+    }
+}
 
+function newGame(){
+    location.reload();
+}
 
 
 
